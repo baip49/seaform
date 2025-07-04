4ceeefec128162d820891425086b8b4ebc01eb36
Cesar Sanchez <byp3ngu1n@gmail.com>
2025-07-04 13:06:14 -0600

    Actualizar formulario de pre-inscripción: mejorar estructura y validaciones

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,25 +1,54 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css',
 })
 export class FormComponent {
   title = 'SEA - COBACH';
 
-  data = {
-    primeraVez: '',
-    matricula: '',
-    sede: '',
-    correo: '',
-    telefono: '',
-    nacionalidad: '',
+  formulario: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formulario = this.fb.group({
+      primeraVez: [''],
+      matricula: ['', [Validators.minLength(15)]],
+      sede: [''],
+      correo: [''],
+      telefono: [''],
+      nacionalidad: [''],
+    });
+  }
+
+  primeraVez(value: string) {
+    const matriculaControl = this.formulario.get('matricula');
+
+    if (value === 'si') {
+      console.log('Sí, es mi primera vez estudiando un bachillerato');
+      matriculaControl?.disable();
+      matriculaControl?.setValue('');
+    }
+    if (value === 'no-cobach') {
+      console.log('No, he estudiado antes en COBACH');
+      matriculaControl?.enable();
+    }
+    if (value === 'no-otra') {
+      console.log('No, vengo de una escuela diferente a COBACH');
+      matriculaControl?.disable();
+    }
   }
 
   submit() {
-    console.log('Datos del formulario', this.data);
+    if (this.formulario.valid) {
+      console.log('Datos del formulario', this.formulario.value);
+    }
   }
 }
